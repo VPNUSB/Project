67b8a2816b46a7b24a199a776b796990ef19aa9a
VPNUSB <38158931+VPNUSB@users.noreply.github.com>
2018-04-07 17:47:29 +0200

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -47,7 +47,40 @@ $(".dropdown").on("hide.bs.dropdown", function(event){
 
 });
 
+function handleFileSelect(event) { //selected file
+	$(".upload-group").show();
+	selectedFile = event.target.files[0];
+};
 
+function confirmUpload() {
+	var storageRef = firebase.storage().ref('/dodImages/');
+	var metadata = {
+		contentType: 'image',
+		customMetadata: {
+			'dogType': 'Lab',
+			'uploadedBy': user.uid,
+			'title': $("#imgTitle").val(),
+			'caption': $("#imgDesc").val()
+		},
+	};
+	var uploadTask = firebase.storage().ref().child('dogImages/' + selectedFile.name).put(selectedFile, metadata);
+	// Register three observers:
+	// 1. 'state_changed' observer, called any time the state changes
+	// 2. Error observer, called on failure
+	// 3. Completion observer, called on successful completion
+	uploadTask.on('state_changed', function(snapshot){
+  		// Observe state change events such as progress, pause, and resume
+  		// See below for more detail
+	}, function(error) {
+  		// Handle unsuccessful uploads
+	}, function() {
+  		// Handle successful uploads on complete
+  		// For instance, get the download URL: https://firebasestorage.googleapis.com/...
+  		$(".upload-group")[0].before("Success!");
+  		$(".upload-group").hide();
 
+	});
+
+}
 
 
