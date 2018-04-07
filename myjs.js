3d009a351e56d89ccd02e8db5baaa939311673cd
VPNUSB <38158931+VPNUSB@users.noreply.github.com>
2018-04-07 18:10:18 +0200

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -37,7 +37,7 @@ function showWelcomeContainer() {
 };
 
 $(".dropdown").on("hide.bs.dropdown", function(event){
-	var storageRef = firebase.storage().ref();
+
     var text = $(event.relatedTarget).text(); // Get the text of the element
     $("#dogDrop").html(text+'<span class="caret"></span>');
     firebase.database().ref('Users/' + user.uid).set({
@@ -54,6 +54,7 @@ function handleFileSelect(event) {
 };
 
 function confirmUpload() {
+	var storageRef = firebase.storage().ref();
 	var metadata = {
 		contentType: 'image',
 		customMetadata: {
@@ -63,23 +64,36 @@ function confirmUpload() {
 			'caption': $("#imgDesc").val()
 		},
 	};
-	var uploadTask = firebase.storage().ref().child('dogImages/' + selectedFile.name).put(selectedFile, metadata);
+	
+	var uploadTask = storageRef.child('dogImages/' + selectedFile.name).put(selectedFile, metadata);
 	// Register three observers:
 	// 1. 'state_changed' observer, called any time the state changes
 	// 2. Error observer, called on failure
 	// 3. Completion observer, called on successful completion
 	uploadTask.on('state_changed', function(snapshot){
-  		// Observe state change events such as progress, pause, and resume
-  		// See below for more detail
+	  // Observe state change events such as progress, pause, and resume
+	  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+	  var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+	  console.log('Upload is ' + progress + '% done');
+	  switch (snapshot.state) {
+	    case firebase.storage.TaskState.PAUSED: // or 'paused'
+	      console.log('Upload is paused');
+	      break;
+	    case firebase.storage.TaskState.RUNNING: // or 'running'
+	      console.log('Upload is running');
+	      break;
+	  }
 	}, function(error) {
-  		// Handle unsuccessful uploads
+	  // Handle unsuccessful uploads
 	}, function() {
-  		// Handle successful uploads on complete
-  		// For instance, get the download URL: https://firebasestorage.googleapis.com/...
-  		$(".upload-group")[0].before("Success!");
-  		$(".upload-group").hide();
-
+	  // Handle successful uploads on complete
+	  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+	  var downloadURL = uploadTask.snapshot.downloadURL;
+	  $(".upload-group")[0].before("Success!");
+	  $(".upload-group").hide();
 	});
+
+
 
 }
 
