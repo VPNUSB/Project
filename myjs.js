591860024a31011c16af7d56cf6be426bd9a1a7d
VPNUSB <38158931+VPNUSB@users.noreply.github.com>
2018-04-09 14:09:26 +0200

    fresh

--- myjs.js
+++ myjs.js
@@ -5,10 +5,8 @@ var selectedFile;
 
 $( document ).ready(function() {
 	$("#welcome").hide();
-	
+	$(".upload-group").hide();
 	document.getElementById("upload").addEventListener('change', handleFileSelect(), false);
-	
-
 });
 
 function signIn() {
@@ -50,19 +48,12 @@ $(".dropdown").on("hide.bs.dropdown", function(event){
 });
 
 function handleFileSelect(event) {
-	
+	$(".upload-group").show();
 	selectedFile = event.target.files[0];
 };
 
-
 function confirmUpload() {
-	
-	
-	var fileName = selectedFile.name;
-	var storageRef = firebase.storage().ref('/dogImages/' + fileName);
-	var uploadTask = storageRef.put(selectedFile);
-
-	/*var metadata = {
+	var metadata = {
 		contentType: 'image',
 		customMetadata: {
 			'dogType': 'Lab',
@@ -70,16 +61,20 @@ function confirmUpload() {
 			'title': $("#imgTitle").val(),
 			'caption': $("#imgDesc").val()
 		},
-	};*/
-	
-	
+	};
+	var uploadTask = firebase.storage().ref().child('dogImages/' + selectedFile.name).put(selectedFile, metadata);
+	// Register three observers:
+	// 1. 'state_changed' observer, called any time the state changes
+	// 2. Error observer, called on failure
+	// 3. Completion observer, called on successful completion
 	uploadTask.on('state_changed', function(snapshot){
-  		
+  		// Observe state change events such as progress, pause, and resume
+  		// See below for more detail
 	}, function(error) {
-  		
+  		// Handle unsuccessful uploads
 	}, function() {
-  		var downloadURL = uploadTask.snapshot.downloadURL;
-  		console.log(downloadURL);
+  		// Handle successful uploads on complete
+  		// For instance, get the download URL: https://firebasestorage.googleapis.com/...
   		$(".upload-group")[0].before("Success!");
   		$(".upload-group").hide();
 
