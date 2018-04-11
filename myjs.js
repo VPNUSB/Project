4f9db279718b125f3d1e5fcf26380b0bcceea83d
VPNUSB <38158931+VPNUSB@users.noreply.github.com>
2018-04-11 22:36:37 +0200

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -89,7 +89,7 @@ uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
 , function() {
   // Upload completed successfully, now we can get the download URL
   var downloadURL = uploadTask.snapshot.downloadURL;
-  var postKey = firebase.database.ref('Posts/').push.key;
+  var postKey = firebase.database.ref('Posts/').push().key;
   var updates = {};
   var postData = {
   	url: downloadURL,
